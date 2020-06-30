9249ce2b2efa40fed78b8f3190ca70c708bb7ef7
Masha <masha.mimishka@gmail.com>
2020-06-30 22:36:53 +0300

    Переменная Name

--- Mysite/routes.py
+++ Mysite/routes.py
@@ -1,7 +1,10 @@
 from Mysite import app
-
+from markupsafe import escape
 
 @app.route('/')
-@app.route('/main')
 def index():
-    return '<h1>Добро пожаловать на мой сайт!</h1>'
+    return '<h1>Добро пожаловать на мой сайт!</h1><p>ты хто?</p><ul><li><a href="/hello/Маша">Маша</a></li><li><a href="/hello/Cаша">Cаша</a></li></ul>'
+
+@app.route('/hello/<name>')
+def hello(name):
+    return '<h1>Привет, %s!</h1>' % escape(name)
