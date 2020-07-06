d3893e6a55f8684f0c86a4459526b4e26d8a09fd
Masha <masha.mimishka@gmail.com>
2020-07-06 21:09:44 +0300

    Finish user login

--- mysite/routes.py
+++ mysite/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for
-from mysite import app
-from mysite.forms import LoginForm
+from mysite import app, db
+from mysite.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user
 from mysite.models import User
 
@@ -45,3 +45,21 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/sign_up', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Вы успешно зарегестрировались!', 'success')
+        return redirect(url_for('index'))
+
+
+    return render_template('register.html', title='Регистрация', form=form)
